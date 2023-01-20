6743a5d2e130d503562c2e04e98d8154f929878c
Nirmalkumar <nirmalkumar.yuvaraj@syncfusion.com>
2023-01-20 15:07:12 +0530

    Updated project

---
+++ DataGridMAUI/DataGridMAUI/Converter/ColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridMAUI
+{
+    public class ColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int _value = (int)value;
+            if (_value >= 45)
+                return Colors.Green;
+            return Colors.Red;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+
+    }
+}
